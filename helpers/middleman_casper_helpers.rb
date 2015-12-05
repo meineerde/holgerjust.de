954fdf872f92432bd17e000cd91b71990227b58a
Holger Just <hello@holgerjust.de>
2015-12-05 23:57:53 +0100

    Put the page title before the blog name

--- helpers/middleman_casper_helpers.rb
+++ helpers/middleman_casper_helpers.rb
@@ -3,15 +3,14 @@ require 'digest/md5'
 
 module MiddlemanCasperHelpers
   def page_title
-    title = blog_settings.name.dup
     if is_tag_page?
-      title << ": #{current_resource.metadata[:locals]['tagname']}"
+      title = current_resource.metadata[:locals]['tagname']
     elsif current_page.data.title
-      title << ": #{current_page.data.title}"
+      title = current_page.data.title
     elsif is_blog_article?
-      title << ": #{current_article.title}"
+      title = current_article.title
     end
-    title
+    [title, blog_settings.name].compact.join(' // ')
   end
 
   def page_description
