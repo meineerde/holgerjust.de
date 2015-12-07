aa03872805d1d2aa4a102a31f0f6b9e45d954b9c
Holger Just <hello@holgerjust.de>
2015-12-07 13:38:14 +0100

    Index articles by year

--- config.rb
+++ config.rb
@@ -38,9 +38,9 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}/{title}.html"
   # Matcher for blog source files
-  blog.sources = 'articles/{year}-{month}-{day}-{title}.html'
+  blog.sources = 'articles/{year}/{title}.html'
   blog.taglink = 'tag/{tag}.html'
   blog.layout = "layout"
   blog.summary_separator = /(READMORE)/
